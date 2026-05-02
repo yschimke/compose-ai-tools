110d4330542cb966d00850da40a97217943ec7d8
Yuri Schimke <yuri@schimke.ee>
2026-05-02 18:10:57 +0100

    Add a11y touch target data product

--- daemon/android/src/main/kotlin/ee/schimke/composeai/daemon/RenderEngine.kt
+++ daemon/android/src/main/kotlin/ee/schimke/composeai/daemon/RenderEngine.kt
@@ -259,6 +259,7 @@ class RenderEngine(
                   previewId = spec.outputBaseName,
                   findings = a11yResult.findings,
                   nodes = a11yResult.nodes,
+                  density = spec.density,
                   pngFile = outputFile,
                   isRound = isRound,
                   imageProcessors = imageProcessors,
